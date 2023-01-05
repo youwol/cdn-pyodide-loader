d3c46578bda1581874d090f90c4a6d109806735e
youwol-guillaume <greinisch@youwol.com>
2023-01-05 10:56:26 +0100

    ✅ remove tests as pyodide's micropip is actually doing the installation

---
+++ src/tests/fake.test.ts
@@ -0,0 +1,6 @@
+test('fake test', async () => {
+    /*
+    Considering we are using micropip from pyodide to load module, there is very little to test here.
+     */
+    expect(true).toBeTruthy()
+})
